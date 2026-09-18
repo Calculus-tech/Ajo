07b31e9817dabe8c9dbbb39173ac820e39af3a28
Ayomide <ayoj9886@gmail.com>
2026-09-17 21:39:02 -0700

    Add custom logo

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { usePollar, WalletButton } from "@pollar/react";
-import Image from "next/image";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
